e6ee98803d80fdeaa58d918f9cb8ef63547401b9
esterdsilva <rm97798@fiap.com.br>
2023-10-18 09:47:58 -0300

    pages dos produtos criadas

---
+++ src/app/produto/camisa/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Camisa() {
+  return (
+    <div>
+        <h1>Camisa</h1>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, dolorum voluptatem. Rem dolor quia facilis repudiandae nobis voluptatem vitae? Cumque quo ullam dolorum fugiat est officia sint quis. Fugit, modi?</p>
+            <figure>
+                <Image
+                    src="https://prints.ultracoloringpages.com/551ada1f48c923acddbbb135962eebb9.png"
+                    alt="Camisa"
+                    width={300}
+                    height={300}
+                    />
+            </figure>
+            <p><Link href="/">Voltar...</Link></p>
+        </div>
+    </div>
+  )
+}
